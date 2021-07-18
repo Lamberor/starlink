9bd8a95063d5489591ef832329793445cff1e77d
Qiuyang <lamberor@gmail.com>
2021-07-18 23:07:53 +0800

    setup header and footer stylesheet

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        ©2021 StarLink Tracker. All Rights Reserved. Website Made by <a href="https://github.com/Lamberor/starlink">Cedric Lai</a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
