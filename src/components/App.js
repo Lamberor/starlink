c8945f8a5d751ce7e9772bf0f12d432343cf38f1
Qiuyang <lamberor@gmail.com>
2021-07-19 00:39:17 +0800

    setup satellite settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
